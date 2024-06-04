f1d943b6c55c49006fd79f3a6b68b81788519086
Neha03N <nikhadeneha4@gmail.com>
2024-06-04 22:01:09 +0530

    Updates

---
+++ precache-manifest.a48880f06e5e88404cb38ef4252fbbb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe36079a72d65d7813cae92e1d75e96c",
+    "url": "/object/index.html"
+  },
+  {
+    "revision": "3fbcbdb050b09124f4bd",
+    "url": "/object/static/css/main.af52fed7.chunk.css"
+  },
+  {
+    "revision": "15fac1b19d0d3d789a48",
+    "url": "/object/static/js/2.37bbdd3e.chunk.js"
+  },
+  {
+    "revision": "74f6145a49ba54ec59c98ae6568fb9bb",
+    "url": "/object/static/js/2.37bbdd3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fbcbdb050b09124f4bd",
+    "url": "/object/static/js/main.d225e326.chunk.js"
+  },
+  {
+    "revision": "d7f5b994f09b474189ea",
+    "url": "/object/static/js/runtime-main.2e0cf2bd.js"
+  }
+]);
